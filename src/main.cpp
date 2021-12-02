1f3f0385a3178a2732f31d37b40fd43d4fe62612
Alexander Novikov <nas133@gmail.com>
2021-12-02 11:45:34 -0600

    4 sensors

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,123 @@
 #include <Arduino.h>
+#include <Wire.h>
+
+//Click here to get the library: http://librarymanager/All#SparkFun_VCNL4040
+#include "SparkFun_VCNL4040_Arduino_Library.h"
+VCNL4040 proximitySensor;
+#define NUMBER_OF_SENSORS 4
+
+#define MUX_ADDR 0x70 //7-bit unshifted default I2C Address
+int LED_pin = 13;
+//Enables a specific port number
+void enableMuxPort(byte portNumber)
+{
+  if (portNumber > 7) portNumber = 7;
+
+  Wire.beginTransmission(MUX_ADDR);
+  //Read the current mux settings
+  Wire.requestFrom(MUX_ADDR, 1);
+  if (!Wire.available()) return; //Error
+  byte settings = Wire.read();
+
+  //Set the wanted bit to enable the port
+  settings |= (1 << portNumber);
+
+  Wire.write(settings);
+  Wire.endTransmission();
+} 
+
+//Disables a specific port number
+void disableMuxPort(byte portNumber)
+{
+  if (portNumber > 7) portNumber = 7;
+
+  Wire.beginTransmission(MUX_ADDR);
+  //Read the current mux settings
+  Wire.requestFrom(MUX_ADDR, 1);
+  if (!Wire.available()) return; //Error
+  byte settings = Wire.read();
+
+  //Clear the wanted bit to disable the port
+  settings &= ~(1 << portNumber);
+
+  Wire.write(settings);
+  Wire.endTransmission();
+} 
 
 void setup() {
   // put your setup code here, to run once:
+   Serial.begin(115200);
+   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.println("SparkFun VCNL4040 Example");
+
+//  Wire.begin(); //Join i2c bus
+
+ /* if (proximitySensor.begin() == false)
+  {
+    Serial.println("Device not found. Please check wiring.");
+    while (1); //Freeze!
+  } */
+  
+/*for (byte x = 0; x < NUMBER_OF_SENSORS; x++)
+  {
+    enableMuxPort(x); //Tell mux to connect to port X
+    disableMuxPort(x);
+} */
+
 }
 
+
 void loop() {
+ unsigned int proxValue; 
   // put your main code here, to run repeatedly:
+   //Get proximity value. The value ranges from 0 to 65535
+  //so we need an unsigned integer or a long.
+  //unsigned int proxValue = proximitySensor.getProximity(); 
+
+ enableMuxPort(0);
+  proximitySensor.begin();
+  proxValue = proximitySensor.getProximity();
+  Serial.print("Proximity Value 0: ");
+  Serial.print(proxValue);
+  Serial.println();
+  disableMuxPort(0);
+
+  enableMuxPort(1);
+  proximitySensor.begin();
+  proxValue = proximitySensor.getProximity();
+  Serial.print("Proximity Value 1: ");
+  Serial.print(proxValue);
+  Serial.println();
+  disableMuxPort(1);
+
+  enableMuxPort(2);
+  proximitySensor.begin();
+  proxValue = proximitySensor.getProximity();
+  Serial.print("Proximity Value 2: ");
+  Serial.print(proxValue);
+  Serial.println();
+  disableMuxPort(2);
+
+  enableMuxPort(3);
+  proximitySensor.begin();
+  proxValue = proximitySensor.getProximity();
+  Serial.print("Proximity Value 3: ");
+  Serial.print(proxValue);
+  Serial.println();
+  disableMuxPort(3); 
+
+
+  digitalWrite(LED_pin, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_pin, LOW);
+   digitalWrite(LED_BUILTIN, LOW);
+  delay(500); 
 }
+
+
+
+
+
+
+
